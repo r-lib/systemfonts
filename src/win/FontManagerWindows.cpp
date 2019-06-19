6908cbb018e3301ea18bcae98effc26a443ce675
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-06-19 13:15:56 +0200

    Force arial regular to front

--- src/win/FontManagerWindows.cpp
+++ src/win/FontManagerWindows.cpp
@@ -123,6 +123,16 @@ int scan_font_dir() {
   delete[] value_data;
   FT_Done_FreeType(library);
 
+  // Move Arial Regular to front
+  for (ResultSet::iterator it = font_list->begin(); it != font_list->end(); it++) {
+    if (strcmp((*it)->family, "Arial") == 0 && strcmp((*it)->style, "Regular") == 0) {
+      FontDescriptor* arial = *it;
+      font_list->erase(it);
+      font_list->insert(font_list->begin(), arial);
+      break;
+    }
+  }
+
   return 0;
 }
 
