9ba1117c2eabc8d5d73598ef8c4bf209c843ff48
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-03-23 12:40:12 +0100

    Commit remaining file as well :-|

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,18 @@
 .onLoad <- function(...) {
+  .Call("sf_init_c", asNamespace("systemfonts"))
   load_emoji_codes()
+}
+
+warn_env <- new.env(parent = emptyenv())
+warn_env$warned <- FALSE
+
+get_fallback <- function() {
+  if (!warn_env$warned) {
+    warning("No fonts detected on your system. Using an empty font.", call. = FALSE)
+    warn_env$warned <- TRUE
+  }
+  list(
+    system.file("unfont.ttf", package = "systemfonts"),
+    0L
+  )
 }
