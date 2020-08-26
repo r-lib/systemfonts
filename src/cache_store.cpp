0cd1b3a7004486358bd618961003a6addc951723
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-08-26 21:22:15 +0200

    Pass errorcode on with face fetching

--- src/cache_store.cpp
+++ src/cache_store.cpp
@@ -4,18 +4,20 @@
 #include "caches.h"
 #include "utils.h"
 
-FT_Face get_cached_face(const char* file, int index, double size, double res) {
+FT_Face get_cached_face(const char* file, int index, double size, double res, int* error) {
   FT_Face face = nullptr;
   BEGIN_CPP
   
   FreetypeCache& cache = get_font_cache();
   if (!cache.load_font(file, index, size, res)) {
+    *error = cache.error_code;
     return face;
   }
   face = cache.get_face();
   
   END_CPP
   
+  *error = 0;
   return face;
 }
 
--- src/cache_store.h
+++ src/cache_store.h
@@ -4,7 +4,7 @@
 #include FT_FREETYPE_H
 #include <cpp11/R.hpp>
 
-FT_Face get_cached_face(const char* file, int index, double size, double res);
+FT_Face get_cached_face(const char* file, int index, double size, double res, int* error);
 
 [[cpp11::init]]
 void export_cache_store(DllInfo* dll);
