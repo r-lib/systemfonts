d959a15749b4c250e5f550ab8afe3f6a52caa7d7
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-12-16 13:46:59 +0100

    Update emoji data

--- update_emoji_codes.R
+++ update_emoji_codes.R
@@ -1,4 +1,4 @@
-uc_data <- 'https://unicode.org/Public/emoji/12.1/emoji-data.txt'
+uc_data <- 'https://unicode.org/Public/13.0.0/ucd/emoji/emoji-data.txt'
 cp <- read.table(uc_data, header = FALSE, sep = ';', stringsAsFactors = FALSE)
 cp <- split(strsplit(trimws(cp[[1]]), '..', fixed = TRUE), trimws(cp[[2]]))
 emoji <- unlist(lapply(cp$Emoji, function(x) {
@@ -17,7 +17,7 @@ base_mod <- unlist(lapply(cp$Emoji_Modifier_Base, function(x) {
   if (length(x) == 1) return(x)
   seq.int(x[1], x[2])
 }))
-base_mod_emoji <- intersect(text_pres, base_mod)
+base_mod_emoji <- intersect(text_pres_emoji, base_mod)
 all_emoji <- unique(unlist(lapply(unlist(cp, recursive = FALSE, use.names = FALSE), function(x) {
   x <- strtoi(paste0('0x', x))
   if (length(x) == 1) return(x)
@@ -27,7 +27,7 @@ all_emoji <- as.integer(all_emoji)
 text_pres_emoji <- as.integer(text_pres_emoji)
 base_mod_emoji <- as.integer(base_mod_emoji)
 
-save(all_emoji, text_pres_emoji, base_mod_emoji, file = 'R/sysdata.rda')
+save(all_emoji, text_pres_emoji, base_mod_emoji, file = 'R/sysdata.rda', version = 2L)
 
 rm(uc_data, cp, emoji, pres, text_pres_emoji, base_mod, base_mod_emoji, all_emoji)
 
