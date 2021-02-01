4ba85d04da27711737c21bdecc66672b8a335275
Thomas Lin Pedersen <thomasp85@gmail.com>
2021-02-01 09:41:48 +0100

    Add issue bullets

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
+release_bullets <- function() {
+  c(
+    '`rhub::check_on_solaris(env_vars = c("_R_CHECK_FORCE_SUGGESTS_" = "false"))`',
+    '`rhub::check_with_valgrind(env_vars = c(VALGRIND_OPTS = "--leak-check=full --track-origins=yes"))`'
+  )
+}
+
 .onLoad <- function(...) {
   load_emoji_codes()
 }
